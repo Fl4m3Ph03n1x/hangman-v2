488e7792892fd4000e99bcef66e5ddd789b56e78
Pedro Martins <palmelapedro@gmail.com>
2022-01-10 09:05:31 -0800

    added alphabet component

--- live_app/lib/live_app_web/live/game/alphabet.ex
+++ live_app/lib/live_app_web/live/game/alphabet.ex
@@ -5,13 +5,29 @@ defmodule LiveAppWeb.Live.Game.Alphabet do
 
   @spec mount(Socket.t) :: {:ok, Socket.t}
   def mount(socket) do
-    {:ok, socket}
+    letters = Enum.map((?a .. ?z), &<< &1 :: utf8 >>)
+    {:ok, assign(socket, letters: letters)}
   end
 
   @spec render(map) :: Rendered.t
   def render(assigns) do
     ~H"""
-      <p>Alphabet</p>
+      <div class="alphabet">
+        <%= for letter <- assigns.letters do %>
+          <div phx-click="make_move" phx-value-key={letter} class={letter_class(letter, assigns.tally)}>
+            <%= letter %>
+          </div>
+        <% end %>
+      </div>
     """
+  end
+
+  @spec letter_class(String.t, map) :: String.t
+  defp letter_class(letter, tally) do
+    cond do
+      Enum.member?(tally.letters, letter) -> "one-letter correct"
+      Enum.member?(tally.used, letter) -> "one-letter wrong"
+      true -> "one-letter"
+    end
   end
 end
